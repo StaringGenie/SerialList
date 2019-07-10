7c788a02ed49e905189025f341390d4bb2b51d49
Данил Ченцов <danil090598@gmail.com>
2019-07-11 01:05:39 +0300

    Home panel

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,22 @@
         section
             .container
                 h1.ui-title-1 Home
+        section
+            .container
+                .task-list
+                    .task-item(
+                        v-for="task in tasks"
+                        :key="task.id"
+                        :class="{completed: task.completed}"
+                    )
+                        .ui-card.ui-card--shadow  
+                            .task-item__info
+                                .task-item__main-info
+                                    span.ui-label.ui-label--light {{task.whatWatch}}
+                                    span Total Time:
+                                span.button-close
+                                 
+                                           
 </template>
 
 
@@ -10,12 +26,42 @@
 export default {
     data() {
         return {
-            
+            tasks:[
+                {
+                    id: 1,
+                    title: 'Game of Thrones',
+                    description: 'winther is comming...',
+                    whatWatch: 'Serial',
+                    completed: true,
+                    editing: false
+                },
+                 {
+                    id: 2,
+                    title: 'Forest Gump',
+                    description: 'Run, Forest',
+                    whatWatch: 'Film',
+                    completed: false,
+                    editing: false
+                }
+            ]
         }
-    },
+    }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.button-close
+    width 20px
+    height @width
+.task-item__info
+    margin-bottom 20px
+    justify-content space-between
+    display flex
+    align-items center
+.task-item
+    margin-bottom 20px
+    &:last-child
+        margin-bottom 0
+.ui-label
+    margin-right 8px
 </style>
