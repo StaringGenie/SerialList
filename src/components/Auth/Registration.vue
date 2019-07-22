4e9df0803d468df7aa39723382d2940b4a18d1eb
Данил Ченцов <danil090598@gmail.com>
2019-07-23 01:14:57 +0300

    inputs

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -2,7 +2,28 @@
     .content-wrapper
         section
             .container
-                h1.ui-title-1 Registration
+                .auth
+                    .auth__banner
+                     h1.ui-title-1 Banner
+                    .auth__form 
+                        span.ui-title-2 Registration
+                            form
+                                input(
+                                    type="email"
+                                    placeholder="Email"
+                                    v-model="email"
+                                )
+                                input(
+                                    type="password"
+                                    placeholder="Password"
+                                    v-model="password"
+                                )
+                                input(
+                                    type="password"
+                                    placeholder="Repeat Password"
+                                    v-model="repeatPassword"
+                                )
+
 </template>
 
 
@@ -10,12 +31,19 @@
 export default {
     data() {
         return {
-            
+            email: '',
+            password: '',
+            repeatPassword: ''
         }
     },
 }
 </script>
 
 <style lang="stylus" scoped>
+.auth
+    display flex
+.auth__banner,
+.auth__form
+    width 50%
 
 </style>
